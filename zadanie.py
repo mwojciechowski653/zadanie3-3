4d9a8148c88812191ec4cd0933d578e433852b97
mwojciechowski653 <117382239+mwojciechowski653@users.noreply.github.com>
2022-11-03 21:09:55 +0100

    Update zadanie.py

--- zadanie.py
+++ zadanie.py
@@ -1,5 +1,11 @@
-def inc(x):
-    return x + 1
+def div(a,b):
+  if b==0:
+    return "Nie dzieli sie przez zero"
+  else:
+    return a/b
+    
+def test_answer():
+  assert div(10,0)=="Nie dzieli sie przez zero"
 
 def test_answer():
-    assert inc(3) == 4
+  assert div(10,2)==5
